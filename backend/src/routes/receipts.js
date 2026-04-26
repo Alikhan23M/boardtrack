b035ff85588eb3865c538afd10dfff5b454424c4
Ali Khan <164453693+Alikhan23M@users.noreply.github.com>
2026-04-26 17:31:48 +0500

    Delete backend directory

--- backend/src/routes/receipts.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDealReceipt } from "../controllers/receiptsController.js";
-
-const router = express.Router();
-
-router.get("/deals/:dealId", getDealReceipt);
-
-export default router;
